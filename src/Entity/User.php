6378fdbf7adecc289c00a6fb09b472a292745b43
Saidbek <xakimovsaidbek020@gmail.com>
2026-02-05 22:16:18 +0500

    Create User Entity

---
+++ src/Entity/User.php
@@ -0,0 +1,184 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
+use App\Repository\UserRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Serializer\Attribute\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[UniqueEntity('email', message: "Bu {{ value }} email allaqachon mavjud")]
+#[ApiResource(
+    operations: [
+        new GetCollection(),
+        new Post(),
+        new Delete()
+    ],
+
+    normalizationContext: ['groups' => ['user:read']],
+    denormalizationContext: ['groups' => ['user:write']],
+    paginationItemsPerPage: 5
+)]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    #[Groups(['user:read'])]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    #[Groups(['user:read', 'user:write'])]
+    #[Assert\NotBlank(message: "Email bo'sh bo'lmasligi kerak.")]
+    #[Assert\Email(message: "Email formati noto'g'ri")]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 255)]
+    #[Groups(['user:write'])]
+    #[Assert\NotBlank(message: "Parol bo'sh bo'lmasligi kerak.")]
+    private ?string $password = null;
+
+    #[ORM\Column(length: 255)]
+    #[Groups(['user:read', 'user:write'])]
+    #[Assert\NotBlank(message: "Ism bo'sh bo'lmasligi kerak.")]
+    private ?string $givenName = null;
+
+    #[ORM\Column(type: Types::SMALLINT)]
+    #[Groups(['user:read', 'user:write'])]
+    #[Assert\NotBlank(message: "Yosh bo'sh bo'lmasligi kerak.")]
+    #[Assert\Range(min: 18, max: 65)]
+    private ?int $age = null;
+
+    #[ORM\Column(length: 255)]
+    #[Groups(['user:read', 'user:write'])]
+    #[Assert\NotBlank(message: "Gender bo'sh bo'lmasligi kerak.")]
+    #[Assert\Choice(
+        choices: ['male', 'female'],
+        message: "Bu turdagi ma'lumot qabul qilinmaydi."
+    )]
+    private ?string $gender = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    #[Groups(['user:read', 'user:write'])]
+    #[Assert\Regex(
+        pattern: '/^\+\d{9,15}$/',
+        message: "Telefon raqam faqat + va raqamlardan iborat bo'lishi mumkin"
+    )]
+    private ?string $phone = null;
+
+    #[ORM\Column]
+    #[Groups(['user:read'])]
+    private array $roles = ['ROLE_USER'];
+
+    #[ORM\Column]
+    #[Groups(['user:read', 'user:write'])]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getGivenName(): ?string
+    {
+        return $this->givenName;
+    }
+
+    public function setGivenName(string $givenName): static
+    {
+        $this->givenName = $givenName;
+
+        return $this;
+    }
+
+    public function getAge(): ?int
+    {
+        return $this->age;
+    }
+
+    public function setAge(int $age): static
+    {
+        $this->age = $age;
+
+        return $this;
+    }
+
+    public function getGender(): ?string
+    {
+        return $this->gender;
+    }
+
+    public function setGender(string $gender): static
+    {
+        $this->gender = $gender;
+
+        return $this;
+    }
+
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(?string $phone): static
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    public function getRoles(): array
+    {
+        return $this->roles;
+    }
+
+    public function setRoles(array $roles): static
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
